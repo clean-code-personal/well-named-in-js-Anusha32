6beb6ec6a33bf1749f22a9e76944b60e797bbd8d
Anusha32 <anushabr3@gmai.com>
2023-02-20 16:51:51 +0530

    getColors module created

--- app/colorCoder.js
+++ app/colorCoder.js
@@ -1,23 +1,6 @@
 import { MajorColorNames, MinorColorNames } from "./colorNames.js";
-import { ColorPair } from "./colorPair.js";
+import {ColorPair} from "./colorPair.js"
 
-function getColorFromPairNumber(pairNumber)
-{
-	let minorSize = MinorColorNames.length;
-	let majorSize = MajorColorNames.length;
-	
-	if (pairNumber < 1 || pairNumber > minorSize * majorSize)
- 	{
- 		throw `Argument PairNumber:${pairNumber} is outside the allowed range` 
-	}
-	let zeroBasedPairNumber = pairNumber - 1;
-    let majorIndex = parseInt (zeroBasedPairNumber / minorSize);
-    let minorIndex = parseInt(zeroBasedPairNumber % minorSize);
-	let  pair = new ColorPair();
-	pair.majorColor = MajorColorNames[majorIndex];
-	pair.minorColor = MinorColorNames[minorIndex];
-	return pair;
-}
 
 function getPairNumberFromColor(pair)
         {
---
+++ app/getColor.js
@@ -0,0 +1,17 @@
+function getColorFromPairNumber(pairNumber)
+{
+	let minorSize = MinorColorNames.length;
+	let majorSize = MajorColorNames.length;
+	
+	if (pairNumber < 1 || pairNumber > minorSize * majorSize)
+ 	{
+ 		throw `Argument PairNumber:${pairNumber} is outside the allowed range` 
+	}
+	let zeroBasedPairNumber = pairNumber - 1;
+    let majorIndex = parseInt (zeroBasedPairNumber / minorSize);
+    let minorIndex = parseInt(zeroBasedPairNumber % minorSize);
+	let  pair = new ColorPair();
+	pair.majorColor = MajorColorNames[majorIndex];
+	pair.minorColor = MinorColorNames[minorIndex];
+	return pair;
+}
