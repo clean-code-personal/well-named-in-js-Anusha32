75239c1b1ced7bc0041940004f6b8104b82e5b28
Anusha32 <anushabr3@gmai.com>
2023-02-20 17:14:56 +0530

    getColor module created

--- app/colorCoder.js
+++ app/colorCoder.js
@@ -1,38 +1,9 @@
 import { MajorColorNames, MinorColorNames } from "./colorNames.js";
-import {ColorPair} from "./colorPair.js"
+import {ColorPair} from "./colorPair.js";
+import {getColorFromPairNumber, getPairNumberFromColor} from "./getColor.js";
 
-
-function getPairNumberFromColor(pair)
-        {
-        let majorIndex = -1;
-        for (let i = 0; i < MajorColorNames.length; i++)
-            {
-                if (MajorColorNames[i] == pair.majorColor)
-                {
-                    majorIndex = i;
-                    break;
-                }
-            }
-
-        let minorIndex = -1;
-            for (let i = 0; i < MinorColorNames.length; i++)
-            {
-                if (MinorColorNames[i] == pair.minorColor)
-                {
-                    minorIndex = i;
-                    break;
-                }
-            }
-    
-        if (majorIndex == -1 || minorIndex == -1)
-            {
-                throw `Unknown Colors:${pair.toString()}`;
-            }
-
-        return (majorIndex * MinorColorNames.length) + (minorIndex + 1);
-	 }
 	 
-	 function test(){
+function test(){
 			let pairNumber = 4;
             let testPair1 = getColorFromPairNumber(pairNumber);
             console.log(`[In]Pair Number: ${pairNumber},[Out] Colors:${testPair1}`);
